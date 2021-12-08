b1beb38b5a57953b16d3bed9c2bfabe5bcd1affc
JianXF <3109441270@qq.com>
2021-12-09 02:22:34 +0800

    Complete basic task.

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,33 @@
 #include"SSSP.hpp"
 #include"Map.cpp"
+#include"AOE.cpp"
 
 int main(){
-    int vertexNum,edgeNum;
+    int vertexNum,edgeNum,start,end;
     cin>>vertexNum>>edgeNum;
     Map map(vertexNum);
-    map.Init(edgeNum);
-    map.PathSearch();
+    if(map.Init(edgeNum)){
+        map.Floyd();
+    }
+    cin>>start>>end;
+    AOE aoe(vertexNum,start,end);
+    aoe.Init(map.GetOrigin(),map.GetDist(),start);
+    aoe.Find_Imp();
     return 0;
 }
+
+/*
+9 11
+0 1 2 3 4 5 6 7 8
+0 1 6
+0 2 4
+0 3 5
+1 4 1
+2 4 1
+3 5 2
+4 6 9
+4 7 7
+5 7 4
+6 8 2
+7 8 4
+*/
