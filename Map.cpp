e81c6565c191833cc918b090d9fe541459df2350
JianXF <3109441270@qq.com>
2021-12-10 10:56:24 +0800

    Fix some bugs.

--- Map.cpp
+++ Map.cpp
@@ -6,10 +6,10 @@ Map::Map(int VertexNum) {
     Dist = new int* [VertexNum];
     Path = new int* [VertexNum];
     for(int i = 0; i < VertexNum; i++){
-        Origin[i] = new int [VertexNum];
-        Dist[i] = new int [VertexNum];
-        Path[i] = new int [VertexNum];
-        for(int j = 0; j < VertexNum; j++){
+        Origin[i] = new int [VertexNum + 1];
+        Dist[i] = new int [VertexNum + 1];
+        Path[i] = new int [VertexNum + 1];
+        for(int j = 0; j <= VertexNum; j++){
             Origin[i][j] = Dist[i][j] = (i==j) ? 0 : INF ;
             Path[i][j] = -1;
         }
@@ -19,7 +19,7 @@ Map::Map(int VertexNum) {
 
 bool Map::Init(int EdgeNum){
     //读取顶点标号
-    cout<<"Vertex Sign:"<<endl;
+    cout<<">> Vertex Sign:"<<endl;
     for(int i=0;i<VertexNum;i++){
         cin>>Sign[i];
     }
@@ -28,13 +28,13 @@ bool Map::Init(int EdgeNum){
         int _start, _end, _distance;
         cin>>_start>>_end>>_distance;   //读取有向边的起点、终点、路径长度
         if(_end >= VertexNum  || _distance >= INF ){
-            cout<<"Initialize failed."<<endl;
+            cout<<">> Initialize failed.Check your map!"<<endl;
             return false;
         }
         AddEdge(_start, _end, _distance);
     }
     Floyd();  //Running with Floyd
-    cout<<"Initialize success."<<endl;
+    cout<<">> Initialize success."<<endl;
     return true;
 }
 
@@ -42,12 +42,12 @@ void Map::AddEdge(int _start,int _end,int _distance){   //添加有向边
     Origin[_start][_end] = Dist[_start][_end] = _distance;
 }
 
-void Map::Floyd(){   //Floyd算法--多源最短路径
+void Map::Floyd(){   //多源最短路径算法
     for(int k = 0; k < VertexNum; k++){
         for(int i = 0; i < VertexNum; i++){
             for(int j = 0; j < VertexNum; j++){
                 if(Dist[i][j] > Dist[i][k] + Dist[k][j]){
-                    Dist[i][j] = Dist[i][k] + Dist[k][j];  //更新最短路径长度
+                    Dist[i][j] = Dist[i][k] + Dist[k][j];  //更新最短距离
                     Path[i][j] = k;     //记录中间节点
                 }
             }
@@ -55,24 +55,25 @@ void Map::Floyd(){   //Floyd算法--多源最短路径
     }
 }
 
-void Map::MinimalPath(char start, char end){   //搜索最短路径
+bool Map::MinimalPath(char start, char end){   //搜索最短路径
     int _start = -1, _end = -1;
     for(int i = 0; i < VertexNum; i ++){    //找到起点/终点标号
         if(Sign[i] == start)  _start=i;
         if(Sign[i] == end)  _end=i;
     }
     if(_start == -1 || _end == -1){
-        cout<<"No such vertex."<<endl;
-        return;
+        cout<<">> No such vertex!"<<endl;
+        return false;
     }
     if(Dist[_start][_end] >= INF){
-        cout<<"Couldn't find this path."<<endl;
-        return;
+        cout<<">> Couldn't find this path!"<<endl;
+        return false;
     }
-    cout<< "Minimal distance: " <<Dist[_start][_end]<<endl;  //输出最短路径
-    cout<< "Shortest Path: ";
+    cout<< ">> Minimal distance: " <<Dist[_start][_end]<<endl;  //输出最短路径
+    cout<< ">> Shortest Path: ";
     FindPath(_start,_end);
     cout<< "--("<< Dist[Path[_start][_end]][_end]<<")-->" << Sign[_end] <<endl;
+    return true;
 }
 
 void Map::FindPath(int _start,int _end){
@@ -101,9 +102,8 @@ char* Map::GetSign(){
     return Sign;
 }
 
-
-
 void Map::CheckDist(){   //遍历最短路径数组
+    cout<<">> Distance:"<<endl;
     for(int i = 0; i < VertexNum; i++){
         for(int j = 0; j < VertexNum; j++){
             cout<<(Dist[i][j] >= INF ? -1 : Dist[i][j])<<" ";
@@ -114,6 +114,7 @@ void Map::CheckDist(){   //遍历最短路径数组
 }
 
 void Map::CheckPath(){   //遍历中间节点数组
+    cout<<">> Path:"<<endl;
     for(int i = 0; i < VertexNum; i++){
         for(int j = 0; j < VertexNum; j++){
             cout<<Path[i][j]<<" ";
